1afa873c3b57c7ab7cdd67eabb4be6b6e2527b98
jasminex21 <jasminexu7003@icloud.com>
2024-08-07 15:36:51 -0500

    modified setup

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="lyriguessr",
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(where="lyriguessr"),
+    package_dir = {'': 'lyriguessr'}
 )
